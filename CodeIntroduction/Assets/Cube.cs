c6d31811ef7a7da9153bfd39abdfc045c17f7b67
jstar34point5x2 <Kinitawowi12@gmail.com>
2020-11-27 08:09:07 -0600

    Intro Projects

---
+++ CodeIntroduction/Assets/Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float sizeModifier = 2.5f;
+    public string newName = "Cubey42";
+    public bool isRotated = false;
+
+    // Used for initialization
+    void Start()
+    {
+        Debug.Log ("This program started!");
+
+        // Modify size of cube to sizeModifier identified
+        transform.name = ImproveName(newName);
+        transform.localScale *= sizeModifier;
+    }
+
+    // Called once per frame.
+    private void Update()
+    {
+        Debug.Log("Updated stuff");
+        transform.localScale = Vector3.one * sizeModifier;
+
+        if (isRotated)
+            {
+            transform.localEulerAngles = Vector3.one * 45f;
+            } else
+        {
+            transform.localEulerAngles = Vector3.one * 0f;
+        }
+
+    }
+
+    string ImproveName (string originalString)
+    {
+        return "-[" + originalString + "]-";
+    }
+}
---
+++ CodeIntroduction/Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject cubePrefab;
+    public int cubeAmount = 20;
+    public float randomArea = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cubeAmount; i++)
+        {
+            GameObject cubeObject = Instantiate(cubePrefab);
+            cubeObject.transform.position = new Vector3(
+                Random.Range(-randomArea, randomArea),
+                Random.Range(-randomArea, randomArea),
+                Random.Range(-randomArea, randomArea));
+        }
+        Instantiate (cubePrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
