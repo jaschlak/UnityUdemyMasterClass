3e28386f485fbcbfff8a108141050b617c6ec1fb
jstar34point5x2 <Kinitawowi12@gmail.com>
2020-11-29 17:13:20 -0600

    Added inputs and camera views including follow camera

---
+++ UnityInput/Assets/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    // array of game cameras
+    public GameObject[] gameCameras;
+
+    // Camera selection
+    private int gameCameraIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Controller Started");
+        FocusOnCamera(gameCameraIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Keyboard directional input
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Jump has been pressed");
+        }
+
+        if (Input.GetKey("left"))
+        {
+            Debug.Log("Moving Left");
+        }
+
+        if (Input.GetKey("right"))
+        {
+            Debug.Log("Moving Right");
+        }
+
+        // Camera change input
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeCamera(1);
+        } else if (Input.GetMouseButtonDown(1))
+        {
+            ChangeCamera(-1);
+        }
+    }
+
+    void FocusOnCamera(int index)
+    {
+        for (int i = 0; i < gameCameras.Length; i++)
+        {
+            gameCameras[i].SetActive(i == index);
+        }
+    }
+
+    void ChangeCamera(int direction)
+    {
+        gameCameraIndex += direction;
+
+        if (gameCameraIndex >= gameCameras.Length)
+        {
+            gameCameraIndex = 0;
+        }
+
+        if (gameCameraIndex < 0)
+        {
+            gameCameraIndex = gameCameras.Length - 1;
+        }
+
+        FocusOnCamera (gameCameraIndex);
+
+    }
+}
---
+++ UnityInput/Assets/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 3.5f;
+    public float rotatingSpeed = 40f;
+    public float jumpingForce = 300;
+    private bool canJump = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move right
+        if (Input.GetKey("right") | Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        // move left
+        if (Input.GetKey("left") | Input.GetKey("a"))
+        {
+            transform.position += -transform.right * speed * Time.deltaTime;
+        }
+
+        // move forwards
+        if (Input.GetKey("up") | Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        // move backwards
+        if (Input.GetKey("down") | Input.GetKey("s"))
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+        }
+
+        // rotate left
+        if (Input.GetKey("q"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, -rotatingSpeed * Time.deltaTime);
+        }
+
+        // rotate right
+        if (Input.GetKey("e"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
+        }
+
+        // jump
+        if (Input.GetKeyDown("space") && canJump)
+        {
+            GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+            canJump = false;
+        }
+
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Floor")
+        {
+            Debug.Log("Hit the floor");
+        }
+
+        canJump = true;
+
+
+    }
+}
