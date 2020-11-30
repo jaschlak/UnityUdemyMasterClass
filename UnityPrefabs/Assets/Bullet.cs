dcbfcfa783edc3e3036f198b84b9048414423769
jstar34point5x2 <Kinitawowi12@gmail.com>
2020-11-29 21:28:15 -0600

    Finshed prefab instantiation for bullets and explosion events

---
+++ UnityPrefabs/Assets/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float shootingForce = 1500f;
+    public Vector3 shootingDirection;
+    public GameObject explosionPrefab;
+
+    public float lifetime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(shootingDirection * shootingForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "TriggerExplosion")
+        {
+            GameObject explosionObject = Instantiate(explosionPrefab);
+            explosionObject.transform.position = transform.position;
+        }
+        Debug.Log(collision.transform.tag);
+    }
+}
---
+++ UnityPrefabs/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // shoot, click mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // instantiate bullet
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            Bullet bullet = bulletObject.GetComponent<Bullet>();
+
+            // determine shooting direction
+            Vector3 shootingDirection = new Vector3
+                (
+                Random.Range(-.15f, .15f),
+                Random.Range(-.15f, .15f),
+                1f
+                );
+
+            // apply shooting direction unit vector
+            bullet.shootingDirection = shootingDirection.normalized;
+        }
+    }
+}
